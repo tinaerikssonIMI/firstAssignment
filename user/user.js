9bbcc2b88ae32014fbacafb2642a057f3ed6a107
tinaerikssonIMI <tina.eriksson@im.se>
2024-11-01 23:03:01 +0100

    Add ESLint configuration and update code for linting compliance

--- user/user.js
+++ user/user.js
@@ -1,8 +1,8 @@
 
 class User {
   constructor(username, password) {
-    this.username = username 
-    this.password = password 
+    this.username = username;
+    this.password = password;
   }
 }
-module.exports = User 
+module.exports = User;
